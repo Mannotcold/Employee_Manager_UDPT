70ffcedb971f7d55fecd22ca1969e907ad3e5bcf
Mannotcold <man2082002@gmail.com>
2024-07-10 01:22:54 +0700

    api login

--- web/Controller/userController.js
+++ web/Controller/userController.js
@@ -1,5 +1,15 @@
 let handleLogin = (req, res) => {
-return res.status(200).json({message: 'hello'})
+    let username = req.body.username;
+    let password = req.body.password;
+    // check email exist
+    //compare password
+    // return userInfor
+    //access_token: JWT json web token
+    return res.status(200).json({
+        yourUsername: username,
+        yourpassword: password
+
+    })
 }
 
 module.exports = {
