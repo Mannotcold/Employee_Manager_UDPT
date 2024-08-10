bb8fc59042cb571edbcf47f4e15378bb89c478a8
Mannotcold <man2082002@gmail.com>
2024-08-10 18:44:43 +0700

    login

--- web/Controller/adminController.js
+++ web/Controller/adminController.js
@@ -23,6 +23,32 @@ const ViewRequestUser = async function (req, res, next) {
     }
 }
 
+const postApproveRequest = async function (req, res, next) {
+    try {
+        // Lấy các thông tin từ request body
+        let requestId = req.params.id;
+        let status = 'Approved';
+        console.log("sdfdfs",requestId);
+        // Thực hiện câu lệnh SQL để cập nhật status
+        const [results, fields] = await connection.query(
+            `UPDATE REQUEST 
+            SET status = ? 
+            WHERE id_request = ?`,
+            [status, requestId]
+        );
+
+        // Ghi log kết quả của câu lệnh SQL
+        console.log(">>>results: ", results);
+
+        // Chuyển hướng về trang quản lý yêu cầu hoặc trang khác phù hợp
+        return res.redirect('/adminhome/ViewRequests');
+    } catch (error) {
+        console.error(">>>Error: ", error);
+        next(error);
+    }
+};
+
+
 const postUpdateProfile = async function (req, res, next) {
     // Lấy các thông tin từ request body
     let employeeID = req.body.employee_id;
@@ -110,5 +136,5 @@ const getSearch = async function (req, res, next) {
 
 
 module.exports = {
-    ViewProfileUser, getSearch, getUpdateUser, postUpdateProfile, postDeleteUser, ViewRequestUser
+    ViewProfileUser, getSearch, getUpdateUser, postUpdateProfile, postDeleteUser, ViewRequestUser, postApproveRequest
 }
--- web/routes/AdminHome.js
+++ web/routes/AdminHome.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const { getAdminpage, getRegisterpage, postRegisterpage, getedituserpage, postUpdatepage, postDeletepage } = require('../Controller/homeController')
-const { ViewProfileUser, getSearch, getUpdateUser, postUpdateProfile, postDeleteUser, ViewRequestUser } = require('../Controller/adminController')
+const { ViewProfileUser, getSearch, getUpdateUser, postUpdateProfile, postDeleteUser, ViewRequestUser, postApproveRequest } = require('../Controller/adminController')
 
 var router = express.Router();
 
@@ -17,6 +17,8 @@ router.post('/ViewProfile/UpdateProfile', postUpdateProfile);
 
 //Trang quản lý request
 router.get('/request', ViewRequestUser);
+router.post('/request/Approve/:id', postApproveRequest);
+router.get('/request/Disapprove/:id', ViewRequestUser);
 
 
 router.post('/Register/Create_user', postRegisterpage);
